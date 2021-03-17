b977fb36d5449aac473693de68e325f464f88e1a
James Reid <jreidmke@gmail.com>
2021-03-17 13:44:08 -0500

    completed schema

---
+++ mcl-schema.sql
@@ -0,0 +1,85 @@
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR UNIQUE NOT NULL CHECK (position('@' IN email) > 1),
+    password VARCHAR NOT NULL,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR NOT NULL,
+    age INTEGER CHECK (age > 10),
+    gender VARCHAR,
+    city VARCHAR NOT NULL,
+    state VARCHAR(2),
+    country VARCHAR NOT NULL,
+    twitter_username VARCHAR,
+    facebook_username VARCHAR,
+    youtube_username VARCHAR,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE composers(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    complete_name TEXT NOT NULL,
+    birth_year INTEGER,
+    death_year INTEGER,
+    epoch TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE works(
+    id INTEGER PRIMARY KEY,
+    composer_id INTEGER REFERENCES composers(id) ON DELETE CASCADE,
+    title VARCHAR NOT NULL,
+    subtitle VARCHAR,
+    genre TEXT,
+    catalogue TEXT,
+    catalogue_number INTEGER,
+    year_composed INTEGER,
+    year_debuted INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE lists(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE list_items(
+    id SERIAL PRIMARY KEY,
+    list_id INTEGER REFERENCES lists(id) ON DELETE CASCADE,
+    work_id INTEGER REFERENCES works(id) ON DELETE CASCADE,
+    listened_to BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE recordings(
+    id SERIAL PRIMARY KEY,
+    work_id INTEGER REFERENCES works(id) ON DELETE CASCADE,
+    composer_id INTEGER REFERENCES composers(id) ON DELETE CASCADE,
+    recording_title TEXT NOT NULL,
+    label TEXT,
+    release_year INTEGER,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE performers(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    nation_of_origin TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE recording_performers(
+    id SERIAL PRIMARY KEY,
+    recording_id INTEGER REFERENCES recordings(id) ON DELETE CASCADE,
+    performer_id INTEGER REFERENCES performers(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT NULL
+);
---
+++ mcl.sql
@@ -0,0 +1,9 @@
+\echo 'Delete and recreate mcl db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE mcl;
+CREATE DATABASE mcl;
+\c mcl; 
+
+\i mcl-schema.sql
+-- \i mcl-seed.sql
