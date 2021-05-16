7cfadc2aeca00e21dab2653a8315bd1a9e10d30d
James Reid <jreidmke@gmail.com>
2021-05-16 10:55:35 -0500

    correctly encoding works into db

--- models/work.js
+++ models/work.js
@@ -1,6 +1,24 @@
 const db = require("../db");
 const axios = require("axios");
-const data = require("../data.json");
+
+const charDict = {
+    "á": "a",
+    "ä": "a",
+    "č": "c",
+    "ç": "c",
+    "é": "e",
+    "è": "e",
+    "ë": "e",
+    "í": "i",
+    "ò": "o",
+    "ó": "o",
+    "ř": "r",
+    "š": "s",
+    "ü": "u",
+    "ů": "u",
+    "ÿ": "y"
+};
+
 
 class Work {
     static async getAll() {
@@ -10,15 +28,41 @@ class Work {
             FROM composers`
         );
 
-        const ids = idsRes.rows.map(i => i.id);
+        // const ids = idsRes.rows.map(i => i.id);
+        
+        const ids = [196, 5, 203];
 
         for(let id of ids) {
             const resp = await axios.get(`https://api.openopus.org/work/list/composer/${id}/genre/all.json`);
-            const workObj = resp.data.works.map(w => ({id: w.id, title: w.title, subtitle: w.subtitle, genre: w.genre, composer_id: id}));
-            workArr.push(workObj);
+            if(resp.data.works) {
+                for(let w of resp.data.works) {
+                    let workObj = {
+                        id: w.id,
+                        subtitle: w.subtitle,
+                        genre: w.genre,
+                        composer_id: id
+                    };
+                    workObj.title = await this.encodeName(w.title);
+                    workObj.subtitle = await this.encodeName(w.subtitle);
+                    workArr.push(workObj);
+                }
+            }
         };
 
         return workArr.flat();
+    };
+
+    static async encodeName(name) {
+        for(let char of name) {
+            if(char in charDict) {
+                name = name.slice(0, name.indexOf(char)) + charDict[char] + name.slice(name.indexOf(char) + 1)
+            }
+        };
+        return name;
+    };
+
+    static async apostrophe(name) {
+        return name.slice(0, name.indexOf("'")) + "'" + name.slice(name.indexOf("'"));
     }
 }
 
