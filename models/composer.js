d7d2e102e2751d8617de86d67dc88a3627c730ae
James Reid <jreidmke@gmail.com>
2021-05-16 10:19:26 -0500

    starting work on correct db encoding

--- models/composer.js
+++ models/composer.js
@@ -1,14 +1,25 @@
 const db = require("../db");
 const axios = require("axios");
 
+const charDict = {
+    "ü": "u"
+};
+
 class Composer {
     static async getAll() {
         let composerArr = [];
 
-        const alphaString = "abcdefghijklmnopqrstuvwxyz"
+        const alphaString = "s"
 
         for(let a of alphaString) {
             const resp = await axios.get(`https://api.openopus.org/composer/list/name/${a}.json`);
+            for(let c of resp.data.composers) {
+                for(let char of c.complete_name) {
+                    if(char in charDict) {
+                        c.complete_name = c.complete_name.slice(0, c.complete_name.indexOf(char)) + charDict[char] + c.complete_name.slice(c.complete_name.indexOf(char) + 1);
+                    }
+                }
+            }
             if(resp.data.composers) composerArr.push(resp.data.composers);
         };
 
